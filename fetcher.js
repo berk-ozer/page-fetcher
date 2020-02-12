713022dda35de36852bf4513672329a751357bea
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-12 23:22:47 +0000

    set up callback functions to write the data from requested URL into a file

--- fetcher.js
+++ fetcher.js
@@ -3,18 +3,22 @@ const fs = require('fs');
 
 const requestedURL = process.argv[2];
 
-request(requestedURL, (error, response, body) => {
-  if (error) {
-    console.log('Error:', error);
-  } else {
-    console.log('Downloaded and saved XXX bytes to XXX');
-  }
-});
+const makeRequest = (saveData) => {
+  request(requestedURL, (error, response, body) => {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      saveData(body);
+      console.log('Downloaded and saved XXX bytes to XXX');
+    }
+  });
+};
 
-const data = 'dsgsdgsdgsdg';
+const writeToFile = (data) => {
+  fs.writeFile('./index.html', data, (err) => {
+    if (err) throw err;
+    console.log('File has been saved!');
+  })
+};
 
-fs.writeFile('./message.txt', data, (err) => {
-  if (err) throw err;
-  console.log('File has been saved!');
-})
-
+makeRequest(writeToFile);
