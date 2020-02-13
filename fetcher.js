15548936d8abce75bc60ee078dcb23a1f30bd1b2
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-13 00:02:48 +0000

    more error handling

--- fetcher.js
+++ fetcher.js
@@ -1,13 +1,22 @@
 const request = require('request');
 const fs = require('fs');
 
-const requestedURL = process.argv[2];
-const toThisPath = process.argv[3];
+let requestedURL, toThisPath;
+
+if (process.argv[2] && process.argv[3]) {
+  requestedURL = process.argv[2];
+  toThisPath = process.argv[3];
+} else {
+  console.log('You\'re missing a parameter!\nPlease make sure you provide a valid URL and file path.');
+  return;
+}
 
 const makeRequest = (saveData) => {
   request(requestedURL, (error, response, body) => {
     if (error) {
-      console.log(error);
+      console.log('Something is wrong with the URL!\n' + error);
+    } else if (response.statusCode !== 200) {
+      console.log('Sorry, this page does not exist!');
     } else {
       saveData(body);
     }
@@ -15,11 +24,11 @@ const makeRequest = (saveData) => {
 };
 
 const writeToFile = (data) => {
-  fs.writeFile(toThisPath, data, (err) => {
-    if (err) {
-      console.log(err);
+  fs.writeFile(toThisPath, data, (error) => {
+    if (error) {
+      console.log('Something went wrong while writing to file!\n' + error);
     } else {
-      console.log(`Downloaded and saved XXX bytes to ${toThisPath}`);
+      console.log(`Downloaded and saved ${fs.statSync(toThisPath).size} bytes to ${toThisPath}`);
     }
   })
 };
